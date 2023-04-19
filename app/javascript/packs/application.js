7025aafe698f0f6bb4e1a72663cc03b4601c8dc8
Vando Ramos <vandorramos@gmail.com>
2023-04-19 15:17:06 -0300

    Js and Bootstrap errors solved, styling buttons and description layouts

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+// import "@hotwired/turbo-rails"
+// import "controllers"
 import 'bootstrap'
 import '../src/application.css'
+
+require("@rails/ujs").start();
+require("@rails/activestorage").start();
+// require("channels");
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'tasks#index'
 
-  resources :tasks, except: [:show]  
+  resources :tasks, except: %i[show]  
 end
